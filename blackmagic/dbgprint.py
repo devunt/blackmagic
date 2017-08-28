9bf137ac092ab4495bc9e49f986bdd641367e57e
Beomi <latheledusjp@gmail.com>
2017-08-29 00:21:17 +0900

    Add Support for py<3.6

--- blackmagic/dbgprint.py
+++ blackmagic/dbgprint.py
@@ -8,4 +8,7 @@ def dbgprint(*args):
     syntax_tree = ast.parse(caller.line)
     names = syntax_tree.body[0].value.args
     for name in names:
-        print(f'{name.id} = {args.pop()}')
+        try:
+            eval("print(f'{name.id} = {args.pop()}')")
+        except SyntaxError:
+            print('{} = {}'.format(name.id, args.pop()))
